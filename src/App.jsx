3536dbe64f491f38fb74abaa4ddaecece5387d6c
luiscarp <luisc.rodriguezpacheco@gmail.com>
2024-03-20 14:21:09 -0600

    contenido dinamico en cada componente guitarra

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,40 @@
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
-
+import { useState, useEffect } from "react";
+import { db } from "./data/db";
 
 function App() {
+  const [data, setData] = useState([]);
 
-  
+  useEffect(() => {
+    setData(db);
+  }, []);
 
   return (
     <>
-    <Header></Header>
+      <Header></Header>
 
-
-    <main className="container-xl mt-5">
+      <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar></Guitar>
-            <Guitar></Guitar>
-            <Guitar></Guitar>
-            <Guitar></Guitar>
-            <Guitar></Guitar>
-            <Guitar></Guitar>
-            <Guitar></Guitar>
-            <Guitar></Guitar>
+          {data.map((g) => {
+            return <Guitar
+            key={g.id}
+            guitar = {guitar}
             
-
-            
-
+            ></Guitar>;
+          })}
         </div>
-    </main>
+      </main>
 
-
-    <footer className="bg-dark mt-5 py-5">
+      <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
         </div>
-    </footer>
-
+      </footer>
     </>
   );
 }
